ee4e7ddc521b8aa81ac8168dc631100244c3ef60
Pravar <pravargupta2@gmail.com>
2024-11-30 02:00:05 +0530

    updates package json

--- Backend/insertAdminUser.js
+++ Backend/insertAdminUser.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
-import User from './models/User.js';  // Assuming your model is in 'models/User.js'
+import User from './models/User.js';
 
-// Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/employeeDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -10,22 +9,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/employeeDB', {
 .then(() => console.log('MongoDB Connected'))
 .catch((err) => console.log('Error:', err));
 
-// Create and save a new user
 const createUser = async () => {
-  const hashedPassword = await bcrypt.hash('admin', 10);  // Hash the plain password
+  const hashedPassword = await bcrypt.hash('admin', 10); 
 
   const user = new User({
-    username: 'admin',  // Change this to the username you want to insert
-    password: hashedPassword,  // Store the hashed password
+    username: 'admin',
+    password: hashedPassword,
   });
 
   try {
-    await user.save();  // Save the user to the database
+    await user.save();
     console.log('User saved successfully');
   } catch (err) {
     console.error('Error saving user:', err);
   }
 };
-
-// Run the function to insert the user
 createUser();
